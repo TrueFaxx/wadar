7143d2e722d6538cf8c48e777586025a89293b71
Priansh Mittra <81822069+primitt@users.noreply.github.com>
2026-03-07 11:03:55 -0800

    reorder

--- frontend/app.py
+++ frontend/app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder='frontend/templates')
+app = Flask(__name__)
 
 @app.route('/')
 def landing():
     return render_template('landing.html')
+
+@app.route('/static/<path:path>')
+def serve_static(path):
+    return app.send_static_file(path)
 
 @app.route('/wadar')
 def wadar():
