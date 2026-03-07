af01c57f308869e0d7ebc6b7c15c7dda11433656
andy1571 <andy.zhang.3025@gmail.com>
2026-03-07 11:02:08 -0800

    reorder folder

--- frontend/app.py
+++ frontend/app.py
@@ -11,8 +11,4 @@ def wadar():
     return render_template('index.html')
 
 if __name__ == '__main__':
-<<<<<<< HEAD:app.py
     app.run(debug=True, port=5002)
-=======
-    app.run(debug=True)
->>>>>>> ff2cf76437a834004b187375ff2e54d08a34b8c5:frontend/app.py
