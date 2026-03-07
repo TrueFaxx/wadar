cf8e372f8c489d6bb43d1c2bee0aea06498f5fc5
Priansh Mittra <81822069+primitt@users.noreply.github.com>
2026-03-07 09:44:27 -0800

    move files

--- frontend/app.py
+++ frontend/app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='frontend/templates')
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
