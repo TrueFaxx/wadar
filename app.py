9089cfdfe68691f47b749a5cd1633679246780a9
andy1571 <andy.zhang.3025@gmail.com>
2026-03-07 11:00:11 -0800

    ui draft

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def landing():
+    return render_template('landing.html')
+
+@app.route('/wadar')
+def wadar():
     return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5002)
